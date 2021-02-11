a9cb9399959097a0d4a66ae5ca55ba2e5f497157
William Mian <william_mian@yahoo.com.br>
2021-02-11 13:21:32 -0300

    initial

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:bytebank2/screens/dashboard.dart';
+import 'package:flutter/material.dart';
+import 'package:bytebank2/database/app_database.dart';
+
+import 'models/contact.dart';
+
+void main() {
+  runApp(BytebankApp());
+  save(Contact(0, 'Ronaldo', 1000));
+  //save(Contact(0, 'Roberval', 2000)).then((id) {
+  //  findAll().then((contacts) => debugPrint(contacts.toString()));
+  //});
+}
+
+class BytebankApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Colors.green[900],
+          accentColor: Colors.blueAccent[700],
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary,
+          )
+      ),
+      home: Dashboard(),
+    );
+  }
+}
+
